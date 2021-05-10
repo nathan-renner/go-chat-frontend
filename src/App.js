7db517ae320ad024b8778fcbb04a3ca66ae4d870
nathan-renner <50720516+nathan-renner@users.noreply.github.com>
2021-05-10 15:56:34 -0400

    Connected to backend. Started sending messages with button

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
+import { useEffect, useState } from "react";
+import { connectToServer, sendMessage } from "./api";
 import "./App.css";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    connectToServer((msg) => {
+      setMessages([...messages, msg]);
+    });
+  });
+
+  const onSendMessage = (message) => {
+    const response = sendMessage(message);
+  };
+
+  const renderMessages = () => {
+    return messages.map((message, i) => {
+      return (
+        <div className="message-container" key={i}>
+          <div className="message">{message.data}</div>
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="app">
       <div className="window">
         <h1>Go Chat</h1>
         <hr />
-        <div className="messages-container">
-          <div className="message-container me">
-            <div className="message">message</div>
-          </div>
-          <div className="message-container">
-            <div className="message">message</div>
-          </div>
-          <div className="message-container">
-            <div className="message">message</div>
-          </div>
-          <div className="message-container">
-            <div className="message">message</div>
-          </div>
-        </div>
+        <div className="messages-container">{renderMessages()}</div>
         <div className="bottom">
           <hr />
           <div className="input-container">
             <input placeholder="Enter your message" />
-            <button>send</button>
+            <button onClick={() => onSendMessage("Hello again Edgar!")}>
+              send
+            </button>
           </div>
         </div>
       </div>
---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+var socket = new WebSocket("ws://localhost:8080/ws");
+
+let connectToServer = (cb) => {
+  console.log("Connecting...");
+
+  socket.onopen = () => {
+    console.log("Successfully Connected");
+  };
+
+  socket.onmessage = (msg) => {
+    console.log(msg);
+    cb(msg);
+  };
+
+  socket.onclose = (event) => {
+    console.log("Socket Closed Connection: ", event);
+  };
+
+  socket.onerror = (error) => {
+    console.log("Socket Error: ", error);
+  };
+};
+
+let sendMessage = (msg) => {
+  console.log("sending msg: ", msg);
+  socket.send(msg);
+};
+
+export { connectToServer, sendMessage };
